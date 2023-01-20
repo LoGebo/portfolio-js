fce4ea33834861d5fbd1bb4587fb2dd6b8cfdb66
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2023-01-20 02:33:04 +0000

    feat: update portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Daniel Martínez",
+  title: "Web Developer & Data Scientist",
+  email: "jdany041@gmail.com",
+  gitHub: "LoGebo",
+  instagram: "jdany04",
+  linkedIn: "Jesús Daniel Martínez García",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "jdany042",
+  youTube: "gebo1861",
 };
 
 const primaryColor = "#4E567E";
